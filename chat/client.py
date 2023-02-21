a5d11b07790eef3b04a31b2cd9731557093177b1
Kevin Huang <kev.y.huang@gmail.com>
2023-02-21 02:56:06 -0500

    Removed redundancies from client code. Now just listens on input.

--- chat/client.py
+++ chat/client.py
@@ -39,47 +39,19 @@ t1 = receive_messages()
 t1.start()
 while True:
     try:
-        # maintains a list of possible input streams
-        sockets_list = []
-
-        """There are two possible input situations. Either the
-        user wants to give manual input to send to other people,
-        or the server is sending a message to be printed on the
-        screen. Select returns from sockets_list, the stream that
-        is reader for input. So for example, if the server wants
-        to send a message, then the if condition will hold true
-        below. If the user wants to send a message, the else
-        condition will evaluate as true"""
-        # select.select on Windows only supports sockets so have
-        # to use msvcrt to add polling for standard input as per
-        # https://stackoverflow.com/a/46823814
-        if platform.system() == 'Windows':
-            read_sockets, write_socket, error_socket = select.select(
-                sockets_list, [], [], 0.1)
-            import msvcrt
-            if msvcrt.kbhit():
-                read_sockets.append(sys.stdin)
+        usr_input = input()
+        if usr_input == "quit":
+            sys.exit()
         else:
-            sockets_list.append(sys.stdin)
-            read_sockets, write_socket, error_socket = select.select(
-                sockets_list, [], [], 0)
-
-        for socks in read_sockets:
-            usr_input = sys.stdin.readline()
-
-            # Parses the user input to see if it is a valid input
-            match = re.match(r"(\d)\|((\S| )*)", usr_input)
-            if match:
-                op_code, message = int(match.group(1)), match.group(2)
-
-                # Encodes the message
-                output = pack_packet(op_code, message)
-                server.send(output)
-
-                if op_code == 5:
-                    break
-            else:
-                print(ERROR_MSG)
+            if usr_input != '':
+                # Parses the user input to see if it is a valid input
+                match = re.match(r"(\d)\|((\S| )*)", usr_input)
+                if match:
+                    op_code, message = int(match.group(1)), match.group(2)
+                    output = pack_packet(op_code, message)
+                    server.send(output)
+                else:
+                    print(ERROR_MSG)
     except KeyboardInterrupt:
         break
 server.close()
